48a0b1d850d99a8be5c2c99b12ab2936a1bad668
dish0nest2 <nikapon13@gmail.com>
2025-10-20 05:37:51 -0400

    Update psychic.sh and bulls

--- bulls_and_cows.sh
+++ bulls_and_cows.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-function cleanup() {
+function cleanup()
+{
   echo ""
   echo "For quit input q"
   echo "Input number: "
 }
 
+function checknum()
+{
+  if echo $1 | grep -qE '(.).*\1'
+  then
+    return 1
+  else
+    return 0
+  fi
+}
+
 trap cleanup SIGINT
+
+declare -a history
+number=''
 
 while true
 do
   read -p "Input number: " number
   case $number in
-    [0-9])
-        echo "Your number: $number"
+    [0-9][0-9][0-9][0-9])
+        checknum $number
         ;;
     q)
         echo "Bye :)"
-        break;;
+        break
+        ;;
+    *)
+        continue
+        ;;
   esac
 done
