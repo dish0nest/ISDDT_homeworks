e63ed9670285efd401b5e5b137535b675607a98c
dish0nest2 <nikapon13@gmail.com>
2025-10-21 01:38:22 -0400

    psychic.sh DONE

--- psychic.sh
+++ psychic.sh
@@ -3,42 +3,52 @@
 #Отделение логики вычисления статистики
 function statistic()
 {
- total=$(( $1 + $2 ))
+ local total=$(( $1 + $2 ))
+ local arr=("")
  echo "Hit: $(( $1*100/$total ))% Miss: $(( $2*100/$total ))%"
+
+ local arr_len=${#history[@]}
+ local start=$(( arr_len > 10 ? arr_len - 10 : 0 ))
+ local last=("${history[@]:$start}")
+
+ echo -n "Numbers: "
+ for num in "${last[@]}"
+ do
+  echo -ne "$num "               # -n чтобы не переводить строку, -e чтобы интерпретировать цвета
+ done
+ echo ""
  echo "========================================================================="
 }
 
-# Подсвечивание текста
-RED='\e[31m'
-GREEN='\e[32m'
-RESET='\e[0m'
+RED=$'\033[31m'
+GREEN=$'\033[32m'
+RESET=$'\033[0m'
 
-# Переменные для хранения информации о:
-# 1) О шаге
-# 2) О количестве удачных попыток
-# 3) О количестве неудачных попыток
 count=1
 all_hits=0
 all_misses=0
+declare -a history
 
-# Цикл для ввода данных
 while true
 do
   echo "Step: $count"
   guess=$(( 0 + $RANDOM % 10 ))
   read -p "Enter a number (0-9) or q for quit: " number
 
-  # Case для проверки ввода
   case $number in
     [0-9])
         if [ $number -eq $guess ]
         then
           echo -e "${GREEN}Hit! My number:${RESET} $guess"
           ((++all_hits))
+          str="${GREEN}$guess${RESET}"
+          history+=("${str}")
           statistic all_hits all_misses
         else
           echo -e "${RED}Miss! My number:$RESET $guess"
           ((++all_misses))
+          str="${RED}$guess${RESET}"
+          history+=("${str}")
           statistic all_hits all_misses
         fi
         ((++count))
