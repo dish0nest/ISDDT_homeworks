d8b395e8df0f26f2d375855f3428fca01857e61f
dish0nest2 <nikapon13@gmail.com>
2025-10-22 02:18:25 -0400

    stsuf.sh DONE

---
+++ stsuf.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function help()
+{
+  echo "Usage: $0 [dirpath]"
+}
+
+if [ $# -ne 1 ]
+then
+  help
+  exit 1
+fi
+
+dir="$1"
+
+if [ ! -d "$dir" ]
+then
+  echo "No such directory"
+  exit 1
+fi
+
+declare -A suffixes
+
+while IFS= read -r -d '' file
+do
+  base=$(basename "$file")
+
+  if [[ "$base" == .* && "$base" != *.* ]]
+  then
+    suffix="no suffix"
+  elif [[ "$base" =~ ^\.[^.]+$ ]]
+  then
+    suffix="no suffix"
+  elif [[ "$base" == *.* ]]
+  then
+    suf="${base##*.}"
+    suffix=".$suf"
+  else
+    suffix="no suffix"
+  fi
+  ((++suffixes["$suffix"]))
+done < <(find "$dir" -maxdepth 1 -type f -print0)
+
+if [ ${#suffixes[@]} -eq 0 ]
+then
+    echo "No files in $dir"
+    exit 0
+fi
+
+for key in "${!suffixes[@]}"
+do
+    echo "$key: ${suffixes[$key]}"
+done
